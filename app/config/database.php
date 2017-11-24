86ace2fdf5d86b8cf857ebf84405d312a9ba23e0
Juri Matsuda <matsuda.juri@gmail.com>
2017-11-24 20:15:32 +0900

    first commit

---
+++ app/config/database.php
@@ -0,0 +1,10 @@
+'mysql' => array(
+  'driver' => 'mysql',
+  'host' => 'localhost',
+  'database' => 'bbc',
+  'username' => 'root',
+  'password' => 'root',
+  'charsset' => 'utf8',
+  'collation' => 'utf8_unicode_ci',
+  'prefix' => ''
+),
