bbffd1231927f6e93c1b0307c9ee139d559d773a
Juri Matsuda <matsuda.juri@gmail.com>
2017-11-30 17:26:52 +0900

    categoriesテーブルのカラム追加

--- database/migrations/2017_11_24_112755_create_categories_table.php
+++ database/migrations/2017_11_24_112755_create_categories_table.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Database\Migrations\Migration;
+use IlluminateDatabaseSchemaBlueprint;
+use IlluminateDatabaseMigrationsMigration;
 
 class CreateCategoriesTable extends Migration
 {
@@ -13,8 +12,9 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('categories', function ($table) {
             $table->increments('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -26,6 +26,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::drop('categories');
     }
 }
